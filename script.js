647f2b50f7ba519b854aaee98ccd55a3bcf9b3e9
suryaguntusuprapto <127292833+suryaguntusuprapto@users.noreply.github.com>
2024-04-10 17:58:52 +0700

    first Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("ini video saya");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio saya");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar saya");
+});
